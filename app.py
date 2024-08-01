5462b4bea6925f8d05da8224760970f5d1df6baf
Ihar Vauchok <northvolin@gmail.com>
2024-08-01 11:43:32 +0200

    s

--- app.py
+++ app.py
@@ -1,23 +1,37 @@
-from flask import Flask, request, render_template
 import pandas as pd
-from surprise import Dataset, Reader, SVD
-
-app = Flask(__name__)
 
 # Загрузим данные MovieLens
-ratings = pd.read_csv('ratings.csv')
-movies = pd.read_csv('movies.csv')
+try:
+ ratings = pd.read_csv('ratings.csv')
+ print("Ratings columns:", ratings.columns)
+except FileNotFoundError:
+ print("File ratings.csv not found.")
+ ratings = pd.DataFrame()  # или другой способ обработки ошибки
 
-# Создаем объект Reader
-reader = Reader(rating_scale=(0.5, 5.0))
+try:
+ movies = pd.read_csv('movies.csv')
+ print("Movies columns:", movies.columns)
+except FileNotFoundError:
+ print("File movies.csv not found.")
+ movies = pd.DataFrame()  # или другой способ обработки ошибки
 
-# Загружаем данные в формат Surprise
-data = Dataset.load_from_df(ratings[['userId', 'movieId', 'rating']], reader)
+# Проверьте, что нужные столбцы присутствуют
+required_columns = ['userId', 'movieId', 'rating']
+if all(column in ratings.columns for column in required_columns):
+ # Создаем объект Reader
+ reader = Reader(rating_scale=(0.5, 5.0))
 
-# Создаем и обучаем модель SVD
-model = SVD()
-trainset = data.build_full_trainset()
-model.fit(trainset)
+ # Загружаем данные в формат Surprise
+ data = Dataset.load_from_df(ratings[required_columns], reader)
+
+ # Создаем и обучаем модель SVD
+ model = SVD()
+ trainset = data.build_full_trainset()
+ model.fit(trainset)
+else:
+ print(f"Ratings file does not contain required columns: {required_columns}")
+ data = None
+ model = None
 
 @app.route('/')
 def index():
@@ -32,12 +46,15 @@ def recommend():
  user_ratings_dict = {movie_id: rating for movie_id, rating in zip(movies['movieId'], user_ratings)}
 
  predictions = []
- for movie_id in movies['movieId']:
-  if movie_id not in user_ratings_dict:
-   predictions.append((movie_id, model.predict(user_id, movie_id).est))
+ if model:
+  for movie_id in movies['movieId']:
+   if movie_id not in user_ratings_dict:
+    predictions.append((movie_id, model.predict(user_id, movie_id).est))
 
- predictions.sort(key=lambda x: x[1], reverse=True)
- recommended_movies = [movies[movies['movieId'] == movie_id]['title'].values[0] for movie_id, _ in predictions[:10]]
+  predictions.sort(key=lambda x: x[1], reverse=True)
+  recommended_movies = [movies[movies['movieId'] == movie_id]['title'].values[0] for movie_id, _ in predictions[:10]]
+ else:
+  recommended_movies = []
 
  return render_template('recommend.html', movies=recommended_movies)
 
